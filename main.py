ee455804fa7bfe20e0adffd9acf2cd829712c8b3
Jovilero <valerium1@hotmail.es>
2022-10-24 00:20:32 +0200

    update functions

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
-# from signal import pause
 import funciones
 import pandas as pd
 import time
+import os
 
-apellido1="ROCH"
+apellido1="LLopis"
+apellido2="Juan"
 
-url,html=funciones.researcher(Pa1=apellido1)
+url,html=funciones.researcher(Pa1=apellido1, Pa2=apellido2)
 nPages=funciones.getNpages(html)
 
 
 for i in range(int(nPages)+1):
-    print(i)
+    # print(i)
     # time.sleep(1)
     try:
-        url,html=funciones.researcher(Pa1=apellido1,Ppagina=int(i))
+        url,html=funciones.researcher(Pa1=apellido1, Pa2=apellido2, Ppagina=int(i))
         pa=pd.read_html(html)
-    except:
-        pass
+    # if fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\{apellido1}' is False:
+        os.mkdir(fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\{apellido1}')
+        ruta=fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\{apellido1}'
+        
+        os.mkdir(fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\{apellido1}\{apellido2}')
+        ruta=fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\{apellido1}\{apellido2}'
+        print(ruta)
+    except: pass
+
     # pa[4].to_csv(fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\dataframe{i}.csv')
-    print(pa[5].loc[1,1][-4:])
-    pa[2].to_csv(fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\df{apellido1}{i}.csv',mode='w')
-    pa[3].to_csv(fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\df{apellido1}{i}.csv',mode='a')
-    pa[4].to_csv(fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\df{apellido1}{i}.csv',mode='a')
-    pa[5].to_csv(fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\df{apellido1}{i}.csv',mode='a')
-    pa[6].to_csv(fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\df{apellido1}{i}.csv',mode='a')
-    pa[7].to_csv(fr'D:\OneDrive - UPV\3-Ocio\4-Programacion\2-Genealogia\df{apellido1}{i}.csv',mode='a')
-    
-    if (i>10): 
-        break
-
-
+    anyo=pa[5].loc[1,1][-4:]
+    # print(anyo)
+    pa[2].to_csv(fr'{ruta}\{i}_{anyo}.csv', mode='w', index=False, header=False)
+    # pa[3].to_csv(fr'{ruta}\{i}_{anyo}.csv', mode='a', index=False, header=False)
+    pa[4].to_csv(fr'{ruta}\{i}_{anyo}.csv', mode='a', index=False, header=False)
+    pa[5].to_csv(fr'{ruta}\{i}_{anyo}.csv', mode='a', index=False, header=False)
+    pa[6].to_csv(fr'{ruta}\{i}_{anyo}.csv', mode='a', index=False, header=False)
+    pa[7].to_csv(fr'{ruta}\{i}_{anyo}.csv', mode='a', index=False, header=False)
+    # print(pa[3].to_latex)
+    # if (i==1):
+    #     break
 
 #meterlo en sql o otro lugar q se vea bien rapido
 #¿pasar a gedcom?
